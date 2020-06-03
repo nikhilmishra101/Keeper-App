1a8a23fcdfd603a7ebfbf30a2a08680dca756991
IEIrodov <nikhilindia30111999@gmail.com>
2020-06-03 16:19:40 +0530

    Layout complete

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function footer() {
+  return (
+    <footer>
+      <p>Created By MayaDev {new Date().getFullYear()} </p>
+    </footer>
+  );
+}
+
+export default footer;
