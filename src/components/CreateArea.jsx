8505c52d89c8eaf994b0d784d59dd0f76752b3fb
IEIrodov <nikhilindia30111999@gmail.com>
2020-06-09 17:31:11 +0530

    adding hover

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Add from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
