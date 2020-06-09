45cf9f91f93a226f6897fe99b1a7f4e11804cd20
IEIrodov <nikhilindia30111999@gmail.com>
2020-06-09 17:43:42 +0530

    Adding texture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Notes from "@material-ui/icons/SpeakerNotes";
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1>
+        <Notes />
+        Keeper
+      </h1>
     </header>
   );
 }
